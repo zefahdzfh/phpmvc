eb910071bc5bfa5ce1acc5099c1a371dbe73f29e
zefahdzfh <zefakalibata@gmail.com>
2024-07-29 09:10:10 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,7 @@
+
+
+    <h1>About Me</h1>
+    <p>Halo, kenalin gw <?= $data['nama'];?>, umur gw <?= $data['umur'];?> tahun, gw adalah <?= $data['pekerjaan'];?></p>
+
+
+
---
+++ app/views/home/index.php
@@ -0,0 +1,3 @@
+
+    <h1>Selamat datang cuy</h1>
+    
